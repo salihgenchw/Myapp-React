40a70e63aead17af664b0e1775c55a5888bc1a62
Salih Genc <salihgenc34@gmail.com>
2021-05-10 14:19:35 +0300

    sidemenu bitti.

--- src/components/VsSide.js
+++ src/components/VsSide.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import explorerlogo from '../images/explorericon.png';
+import searchlogo from '../images/searchicon.png';
+import giticon from '../images/giticon.png';
+import debugicon from '../images/debugicon.png';
+import expicon from '../images/expicon.png';
+
 import '../css/vsSide.css';
 
 function VsSide() {
@@ -9,8 +14,19 @@ function VsSide() {
                 <a href="/">
                    <img className="mt-3" src={explorerlogo} alt="explorerlogo"/>  
                 </a>
-            </div>
-            
+                <a href="/">
+                   <img className="mt-4" src={searchlogo} alt="explorerlogo"/>  
+                </a>
+                <a href="/">
+                   <img className="mt-4" src={giticon} alt="explorerlogo" style={{width:"30px"}}/>  
+                </a>
+                <a href="/">
+                   <img className="mt-4" src={debugicon} alt="explorerlogo" style={{width:"33px"}}/>  
+                </a>
+                <a href="/">
+                   <img className="mt-4" src={expicon} alt="explorerlogo" style={{width:"32px"}}/>  
+                </a>
+            </div>  
         </div>
     )
 }
